5175b719b4ab6672f9ff88c4ce53a31ca7ff49bc
juanpa3321 <jupablor4@gmail.com>
2026-09-09 23:28:18 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'agenda-pastelera-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  // Nunca cachear llamadas a Google (login/Drive): siempre ir a la red.
+  if (event.request.url.includes('googleapis.com') || event.request.url.includes('google.com')) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
